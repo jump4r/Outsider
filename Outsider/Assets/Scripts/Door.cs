ddd3fc62f1ee9cd42e5192f12d48b0b5d62d41ab
Andrew Morris <jump3r@dggaming.net>
2015-03-18 02:28:18 -0400

    Add basic lock and key functionality.

---
+++ Outsider/Assets/Scripts/Door.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour
+{
+
+    [HideInInspector]
+    public bool unlocked = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OpenDoor()
+    {
+        if (unlocked)
+        {
+            Debug.Log("Play Door Opening Animation.");
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+
+    void CloseDoor()
+    {
+        if (unlocked)
+        {
+            Debug.Log("Play Door Closing Animation.");
+            gameObject.GetComponent<MeshRenderer>().enabled = true;
+            gameObject.GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+
+    // Again, not totally sure how doors will be opening, but I will use triggers until
+    // we figure out what we're doing.
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            OpenDoor();
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            CloseDoor();
+        }
+    }
+}
---
+++ Outsider/Assets/Scripts/Key.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : MonoBehaviour
+{
+
+    public GameObject doorObject;
+    private Door door;
+
+    // Use this for initialization
+    void Start()
+    {
+        door = doorObject.GetComponent<Door>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Note, this key does not need to be a collectable,
+    // When we figure out how we actually want to unlock the doors the code will go here
+    // Until then I'm going to use OnTriggerEnter.
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            door.unlocked = true;
+            Debug.Log("Door Unlocked");
+            Destroy(gameObject);
+        }
+    }
+}
