3e85d40b35dea1e2a772f104b2a5a10be3074f22
kettle11 <ian.kettlewell@gmail.com>
2015-03-18 01:50:49 -0400

    particle tests

---
+++ Outsider/Assets/ParticleScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleScript : MonoBehaviour {
+
+	SphereCollider collider;
+	// Use this for initialization
+	void Start () {
+		collider = GetComponent<SphereCollider>();
+	}
+
+	public float gravity = 5f;
+	public float repelForce = 50f;
+
+	Vector3 velocity;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position += velocity * Time.deltaTime;
+		velocity += new Vector3(0,-gravity,0f) * Time.deltaTime;
+	}
+
+	void OnTriggerStay(Collider other) {
+
+		Vector3 pointOfContact = other.ClosestPointOnBounds(transform.position);
+		Vector3 dif = pointOfContact - transform.position;
+
+		velocity -= ((1f - ((dif).magnitude / collider.radius)) * Time.deltaTime * repelForce) * dif.normalized;
+	}
+}
