557cad6e58e12b271b302272cb6a6d5a319fc105
kettle11 <ian.kettlewell@gmail.com>
2015-03-18 02:18:19 -0400

    further particle tests

--- Outsider/Assets/ParticleScript.cs
+++ Outsider/Assets/ParticleScript.cs
@@ -1,23 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ParticleScript : MonoBehaviour {
 
+	public GameObject target;
+	public GameObject secondaryTarget;
+
 	SphereCollider collider;
+
+	static List<GameObject> allParticles = new List<GameObject>();
+
+	void Awake()
+	{
+		allParticles.Add(this.gameObject);
+	}
 	// Use this for initialization
 	void Start () {
+
 		collider = GetComponent<SphereCollider>();
+
+		if(Random.value > .6f)
+		{
+			target = allParticles[Random.Range(0,allParticles.Count)];
+			secondaryTarget = allParticles[Random.Range(0,allParticles.Count)];
+		}
 	}
 
 	public float gravity = 5f;
 	public float repelForce = 50f;
 
+	public float targetAcceleration = 20f;
+
+	public float maxSpeed = 30f;
+
 	Vector3 velocity;
 
 	// Update is called once per frame
 	void Update () {
+
+		if(velocity.magnitude > maxSpeed)
+		{
+			velocity = maxSpeed * velocity.normalized;
+		}
+
 		transform.position += velocity * Time.deltaTime;
 		velocity += new Vector3(0,-gravity,0f) * Time.deltaTime;
+
+		if(target != null)
+		{
+			velocity += (target.transform.position - transform.position).normalized * targetAcceleration * Time.deltaTime;
+		}
+
+		if(Input.GetKeyDown(KeyCode.T))
+		{
+			target = secondaryTarget;
+		}
+
 	}
 
 	void OnTriggerStay(Collider other) {
