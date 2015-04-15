3e19647f937de2634f31d7e265c83b6cd5493020
jump4r <morria3@rpi.edu>
2015-04-14 20:04:10 -0400

    Added Terrain and basic puzzlies

--- Outsider/Assets/Scripts/Background/Rock.cs
+++ Outsider/Assets/Scripts/Background/Rock.cs
@@ -1,18 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Rock : MonoBehaviour {
+public class Rock : MonoBehaviour
+{
 
-    private float oscSpeed = .1f;
-    private float oscLength = .05f;
+    private float oscSpeed = .3f;
+    private float oscSpeed2 = .6f;
+    private float oscLength = 50;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.position = new Vector3(transform.position.x, transform.position.y + (Mathf.Sin(Time.time * oscSpeed) * oscLength), transform.position.z);
-	}
+    private bool upDirection = true;
+    private Vector3 startPos;
+    private Vector3 endPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        oscSpeed = Random.Range(.2f, .4f);
+        oscSpeed2 = Random.Range(.3f, .6f);
+        oscLength = Random.Range(20, 50);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + (Mathf.Sin(Time.time * oscSpeed) * oscSpeed2) / oscLength, transform.position.z);
+        //transform.position = (upDirection) ? Vector3.Lerp(startPos, endPos, 5f) : Vector3.Lerp(endPos, startPos, 5f);
+
+    }
 }
